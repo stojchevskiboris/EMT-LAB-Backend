8f4c3f6be48761e3ee3cf8304a7e85de4cd1706f
Boris Stojchevski <stojcevskib.02@gmail.com>
2023-04-11 01:27:17 +0200

    Add DataInitializer for h2 db

--- src/main/java/mk/ukim/finki/emtlabs/config/DataInitializer.java
+++ src/main/java/mk/ukim/finki/emtlabs/config/DataInitializer.java
@@ -1,15 +1,14 @@
 package mk.ukim.finki.emtlabs.config;
 
 import lombok.Getter;
-import mk.ukim.finki.emtlabs.model.Author;
-import mk.ukim.finki.emtlabs.model.Country;
 import mk.ukim.finki.emtlabs.service.AuthorService;
 import mk.ukim.finki.emtlabs.service.CountryService;
-import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import mk.ukim.finki.emtlabs.service.UserService;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+
+import static mk.ukim.finki.emtlabs.model.enums.Role.ROLE_ADMIN;
 
 //ONLY IF USING H2 DATABASE
 
@@ -19,15 +18,23 @@ public class DataInitializer {
 
     private final CountryService countryService;
     private final AuthorService authorService;
+    private final UserService userService;
 
-    public DataInitializer(CountryService countryService, AuthorService authorService) {
+    public DataInitializer(CountryService countryService, AuthorService authorService, UserService userService) {
         this.countryService = countryService;
         this.authorService = authorService;
+        this.userService = userService;
     }
 
     @PostConstruct
     public void init() throws Exception {
-        this.countryService.save("Russia", "Europe");
-        this.authorService.save("Lev Nikolaevich", "Tolstoy",1L);
+        if (this.authorService.findAll().isEmpty()){
+            this.countryService.save("Russia", "Europe");
+            this.authorService.save("Lev Nikolaevich", "Tolstoy",1L);
+        }
+        if (this.userService.findByUsername("admin").isEmpty()){
+            this.userService.register("admin","admin","admin","admin","admin",ROLE_ADMIN);
+        }
+
     }
 }
